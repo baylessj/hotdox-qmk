5a2cc6b4d44ee5f46b92988521bd83b36a9e5c80
Jonathan Bayless <baylessj@purdue.edu>
2019-07-14 11:56:13 -0400

    misc fixes

--- keymap.c
+++ keymap.c
@@ -299,44 +299,24 @@ enum custom_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 0: Basic layer
  * .--------------------------------------------------.  .--------------------------------------------------.
- * | Grave  |   !  |   @  |   #  |   $  |   %  |   {  |  |  }   |   ^  |   &  |   *  |   -  |   =  | BSpace |
+ * | Esc    |   1  |   2  |   3  |   4  |   5  |   6  |  |  7   |   8  |   9  |   0  |   -  |   =  |   -    |
  * |--------+------+------+------+------+------+------|  |------+------+------+------+------+------+--------|
- * | Tab    |   Q  |   W  |   E  |   R  |   T  |   (  |  |  )   |   Y  |   U  |   I  |   O  |   P  |   \    |
+ * | Grave  |   Q  |   W  |   E  |   R  |   T  |   (  |  |  )   |   Y  |   U  |   I  |   O  |   P  |   \    |
  * |--------+------+------+------+------+------|      |  |      |------+------+------+------+------+--------|
- * | Esc    |   A  |   S  |   D  |   F  |   G  |------|  |------|   H  |   J  |   K  |   L  |   ;  |   '    |
+ * | L2     |   A  |   S  |   D  |   F  |   G  |------|  |------|   H  |   J  |   K  |   L  |   ;  |   '    |
  * |--------+------+------+------+------+------|   [  |  |  ]   |------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  |      |  |      |   N  |   M  |   ,  |   .  |   /  | RShift |
  * '--------+------+------+------+------+-------------'  '-------------+------+------+------+------+--------'
- *   | LCtl |Super | Alt  | ~L1  |Space |                              | Left | Down | Up   |Right | Del  |
+ *   | LCtl |Super | Alt  | L3   |Shift |                              | Left | Down | Up   |Right | Del  |
  *   '----------------------------------'                              '----------------------------------'
  *                                      .-------------.  .-------------.
- *                                      | Ins  |NumClk|  | Home | End  |
+ *                                      |      |      |  |      |      |
  *                               .------+------+------|  |------+------+------.
- *                               |      |      |CapsLk|  | PgUp |      |      |
- *                               |BSpace| Del  |------|  |------| ~L2  |Enter |
- *                               |      |      | ~L3  |  | PgDn |      |      |
+ *                               |      |      |      |  |      |      |      |
+ *                               | Space|BSpace|------|  |------| Tab  |Enter |
+ *                               |      |      |      |  |      |      |      |
  *                               '--------------------'  '--------------------'
  *
- * Optional overrides: see CFQ_USER_KEY# defines.
- *
- * .--------------------------------------------------.  .--------------------------------------------------.
- * |        |      |      |      |      |      |      |  |      |      |      |      |      |      | USR0   |
- * |--------+------+------+------+------+------+------|  |------+------+------+------+------+------+--------|
- * |        |      |      |      |      |      |      |  |      |      |      |      |      |      |        |
- * |--------+------+------+------+------+------|      |  |      |------+------+------+------+------+--------|
- * |        |      |      |      |      |      |------|  |------|      |      |      |      |      |        |
- * |--------+------+------+------+------+------|      |  |      |------+------+------+------+------+--------|
- * |        |      |      |      |      |      |      |  |      |      |      |      |      |      |        |
- * '--------+------+------+------+------+-------------'  '-------------+------+------+------+------+--------'
- *   |      |      |      | USR1 |      |                              |      |      |      |      | USR8 |
- *   '----------------------------------'                              '----------------------------------'
- *                                      .-------------.  .-------------.
- *                                      | USR2 | USR3 |  |      |      |
- *                               .------+------+------|  |------+------+------.
- *                               |      |      | USR6 |  |      |      |      |
- *                               | USR4 | USR5 |------|  |------|      |      |
- *                               |      |      | USR7 |  |      |      |      |
- *                               '--------------------'  '--------------------'
  */
 
 /* If it accepts an argument (i.e, is a function), it doesn't need KC_.
@@ -345,60 +325,60 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   /* left hand */
   KC_ESC,  KC_1,    KC_2,    KC_3,          KC_4,   KC_5,    KC_6,
   KC_GRV,  KC_Q,    KC_W,    KC_E,          KC_R,   KC_T,    KC_LPRN,
-  CFQ_USER_KEY1,  KC_A,    KC_S,    KC_D,          KC_F,   KC_G,
+  MO(1),  KC_A,    KC_S,    KC_D,          KC_F,   KC_G,
   KC_LSFT, KC_Z,    KC_X,    KC_C,          KC_V,   KC_B,    KC_LBRC,
-  KC_LCTL, KC_LGUI, KC_LALT, CFQ_USER_KEY1, KC_SPC,
-                                                    CFQ_USER_KEY2, CFQ_USER_KEY3,
-                                     K80(L0K0),     K80(L0K1),     CFQ_USER_KEY6,
-                                     KC_SPC,        KC_BSPC,       CFQ_USER_KEY7,
+  KC_LCTL, KC_LGUI, KC_LALT, MO(2), KC_RSFT,
+                                                    KC_NO,         KC_NO,
+                                     K80(L0K0),     K80(L0K1),     KC_NO,
+                                     KC_SPC,        KC_BSPC,       KC_NO,
   /* right hand */
-  KC_7,        KC_8,    KC_9,    KC_0,   KC_EQL,  KC_MINS,   KC_MINS,
+  KC_7,        KC_8,    KC_9,    KC_0,   KC_MINS, KC_EQL,    KC_MINS,
   KC_RPRN,     KC_Y,    KC_U,    KC_I,   KC_O,    KC_P,      KC_BSLS,
                KC_H,    KC_J,    KC_K,   KC_L,    KC_SCLN,   KC_QUOT,
-  KC_RBRC,     KC_N,    KC_M,    KC_COMM,KC_DOT,  KC_SLSH,   KC_RSFT,
+  KC_RBRC,     KC_N,    KC_M,    KC_COMM,KC_DOT,  KC_SLSH,   KC_NO,
                         KC_LEFT, KC_DOWN,KC_UP,   KC_RGHT,   CFQ_USER_KEY8,
-  KC_HOME, KC_END,
-  KC_PGUP, K80(L0K2),  K80(L0K3),
-  KC_PGDN, KC_TAB,     KC_ENT
+  KC_NO, KC_NO,
+  KC_NO, K80(L0K2),  K80(L0K3),
+  KC_NO, KC_TAB,     KC_ENT
 ),
 /* Keymap 1: KeyPad, Macro Record
  *
  * .--------------------------------------------------.  .--------------------------------------------------.
- * |        |      |      |      |      |      |  {}  |  |  }{  |      |NumLck|   /  |   *  |   -  |        |
+ * |        | F10  | F11  | F12  |      |      |  {}  |  |  }{  |      |NumLck|   /  |   *  |   -  |        |
  * |--------+------+------+------+------+------+------|  |------+------+------+------+------+------+--------|
- * |        |      |      |      |      |  =>  |  ()  |  |  )(  |  <=  |   7  |   8  |   9  |   +  |        |
+ * |        |  F7  |  F8  |  F9  |      |  =>  |  ()  |  |  )(  |  <=  |      |   UP  |     |   +  |        |
  * |--------+------+------+------+------+------|      |  |      |------+------+------+------+------+--------|
- * |        |      |      |      |      |  ->  |------|  |------|  <-  |   4  |   5  |   6  |   +  |        |
+ * |        |  F4  |  F5  |  F6  |      |  ->  |------|  |------|  <-  |  LEFT| DOWN | RGHT |   +  |        |
  * |--------+------+------+------+------+------|  []  |  |  ][  |------+------+------+------+------+--------|
- * |        |      |      |      |      |  <>  |      |  |      |  ><  |   1  |   2  |   3  | Enter|        |
+ * |        |  F1  |  F2  |  F3  |      |  <>  |      |  |      |  ><  |      |      |      | Enter|        |
  * '--------+------+------+------+------+-------------'  '-------------+------+------+------+------+--------'
- *   |      |      |      |      |      |                              |   0  |      |   .  | Enter|      |
+ *   |      |      |      |      |      |                              |      |      |   .  | Enter|      |
  *   '----------------------------------'                              '----------------------------------'
  *                                      .-------------.  .-------------.
- *                                      |Start1|Start2|  |      |      |
+ *                                      |      |      |  |      |      |
  *                               .------+------+------|  |------+------+------.
- *                               |      |      | Stop |  |      |      |      |
- *                               |Play1 |Play2 |------|  |------|      |      |
+ *                               |      |      |      |  |      |      |      |
+ *                               |      | DEL  |------|  |------|      |      |
  *                               |      |      |      |  |      |      |      |
  *                               '--------------------'  '--------------------'
  */
 /* KEYPAD & MACRO */
 [LAYER_KPAD] = LAYOUT_ergodox_76_or_80(
   /* left hand */
-  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          M_BRACKET_IN_CBR,
-  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, M_ARROW_REQL,     M_BRACKET_IN_PRN,
-  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, M_ARROW_RMINUS,
-  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, M_BRACKET_IN_ANG, M_BRACKET_IN_BRC,
+  KC_TRNS, KC_F10,  KC_F11,  KC_F12,  KC_TRNS, KC_TRNS,          M_BRACKET_IN_CBR,
+  KC_TRNS, KC_F7,   KC_F8,   KC_F9,   KC_TRNS, M_ARROW_REQL,     M_BRACKET_IN_PRN,
+  KC_TRNS, KC_F4,   KC_F5,   KC_F6,   KC_TRNS, M_ARROW_RMINUS,
+  KC_TRNS, KC_F1,   KC_F2,   KC_F3,   KC_TRNS, M_BRACKET_IN_ANG, M_BRACKET_IN_BRC,
   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-                                               DYN_REC_START1,   DYN_REC_START2,
-                              K80(L1K0),       K80(L1K1),        DYN_REC_STOP,
-                              DYN_MACRO_PLAY1, DYN_MACRO_PLAY2,  KC_TRNS,
+                                               KC_NO,   KC_NO,
+                              K80(L1K0),       K80(L1K1), KC_NO,
+                              KC_NO, KC_DEL,  KC_TRNS,
   /* right hand */
   M_BRACKET_OUT_CBR, KC_TRNS,           KC_NLCK, KC_KP_SLASH, KC_KP_ASTERISK, KC_KP_MINUS,  KC_TRNS,
-  M_BRACKET_OUT_PRN, M_ARROW_LEQL,      KC_KP_7, KC_KP_8,     KC_KP_9,        KC_KP_PLUS,  KC_TRNS,
-                     M_ARROW_LMINUS,    KC_KP_4, KC_KP_5,     KC_KP_6,        KC_KP_PLUS,  KC_TRNS,
-  M_BRACKET_OUT_BRC, M_BRACKET_OUT_ANG, KC_KP_1, KC_KP_2,     KC_KP_3,        KC_KP_ENTER, KC_TRNS,
-                                        KC_KP_0, KC_TRNS,     KC_KP_DOT,      KC_KP_ENTER, KC_TRNS,
+  M_BRACKET_OUT_PRN, M_ARROW_LEQL,      KC_NO,   KC_UP,       KC_NO,          KC_KP_PLUS,  KC_TRNS,
+                     M_ARROW_LMINUS,    KC_LEFT, KC_DOWN,     KC_RGHT,        KC_KP_PLUS,  KC_TRNS,
+  M_BRACKET_OUT_BRC, M_BRACKET_OUT_ANG, KC_NO,   KC_NO,       KC_NO,          KC_KP_ENTER, KC_TRNS,
+                                        KC_NO, KC_TRNS,     KC_KP_DOT,      KC_KP_ENTER, KC_TRNS,
   KC_TRNS, KC_TRNS,
   KC_TRNS, K80(L1K2), K80(L1K3),
   KC_TRNS, KC_TRNS, KC_TRNS
@@ -406,15 +386,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 2: FKeys, media & mouse keys
  *
  * .--------------------------------------------------.  .--------------------------------------------------.
- * |        |      |      |      |      |      |      |  | Mute |      |  F10 |  F11 |  F12 |      |        |
+ * |        |      |      |      |      |      |      |  | Mute |      |      |      |      |      |        |
  * |--------+------+------+------+------+------+------|  |------+------+------+------+------+------+--------|
- * |        |      |      | MsUp |      |      |MWhlUp|  |VolUp |      |  F7  |  F8  |  F9  |      |        |
+ * |        |      |      | MsUp |      |      |MWhlUp|  |VolUp |      |  7   |  8   |  9   |      |        |
  * |--------+------+------+------+------+------|      |  |      |------+------+------+------+------+--------|
- * |        |      |MsLeft|MsDown|MsRght|      |------|  |------|      |  F4  |  F5  |  F6  |      |        |
+ * |        |      |MsLeft|MsDown|MsRght|      |------|  |------|      |  4   |  5   |  6   |      |        |
  * |--------+------+------+------+------+------|MWhlDn|  |VolDn |------+------+------+------+------+--------|
- * |        |      | Rclk | Mclk | Lclk |      |      |  |      |      |  F1  |  F2  |  F3  |      |        |
+ * |        |      | Rclk | Mclk | Lclk |      |      |  |      |      |  1   |  2   |  3   |      |        |
  * '--------+------+------+------+------+-------------'  '-------------+------+------+------+------+--------'
- *   |      |      |      |      |      |                              |      |      |      |      |      |
+ *   |      |      |      |      |      |                              |  0   |      |      |      |      |
  *   '----------------------------------'                              '----------------------------------'
  *                                      .-------------.  .-------------.
  *                                      |      |      |  | MRwd | MFwd |
@@ -436,11 +416,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                            K80(L2K0), K80(L2K1), KC_TRNS,
                            KC_TRNS,   KC_TRNS,   KC_TRNS,
   /* right hand */
-  KC_MUTE,  KC_TRNS, KC_F10,  KC_F11,  KC_F12,  KC_TRNS, KC_TRNS,
-  KC_VOLU,  KC_TRNS, KC_F7,   KC_F8,   KC_F9,   KC_TRNS, KC_TRNS,
-            KC_TRNS, KC_F4,   KC_F5,   KC_F6,   KC_TRNS, KC_TRNS,
-  KC_VOLD,  KC_TRNS, KC_F1,   KC_F2,   KC_F3,   KC_TRNS, KC_TRNS,
-                     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+  KC_MUTE,  KC_TRNS, KC_NO,   KC_NO,   KC_NO,  KC_TRNS, KC_TRNS,
+  KC_VOLU,  KC_TRNS, KC_7,    KC_8,    KC_9,    KC_TRNS, KC_TRNS,
+            KC_TRNS, KC_4,    KC_5,    KC_6,    KC_TRNS, KC_TRNS,
+  KC_VOLD,  KC_TRNS, KC_1,    KC_2,    KC_3,    KC_TRNS, KC_TRNS,
+                     KC_0,    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
   KC_MRWD, KC_MFFD,
   KC_MPRV, K80(L2K2), K80(L2K3),
   KC_MNXT, KC_TRNS, KC_MPLY
